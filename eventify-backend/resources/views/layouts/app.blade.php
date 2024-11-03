bf5dcd7548ab8e178c4d78fd91fd9abaf6d59718
Pablorv004 <pablorv004@gmail.com>
2024-11-03 15:17:57 +0100

    Added home button on dropdown. Fixed register email not sending correctl

--- eventify-backend/resources/views/layouts/app.blade.php
+++ eventify-backend/resources/views/layouts/app.blade.php
@@ -131,6 +131,9 @@
                                             {{ __('Admin Panel') }}
                                         </a>
                                     @endif
+                                    <a class="dropdown-item" href="{{ route('home') }}">
+                                        {{ __('Home') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
