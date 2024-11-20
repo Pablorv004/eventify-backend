8a5e438eda9858f51171f48842180d9100f66b81
RafBel94 <rafael.beltrancaceres@gmail.com>
2024-11-20 10:46:28 +0100

    Style for event_attendees_report

--- eventify-backend/resources/views/reports/event_attendees_report.blade.php
+++ eventify-backend/resources/views/reports/event_attendees_report.blade.php
@@ -1,26 +1,42 @@
-<div class="container">
-    <h1>Event Attendees Report</h1>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Image</th>
-                <th>Title</th>
-                <th>Organizer</th>
-                <th>Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($events as $event)
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .centered-table {
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+        }
+        .centered-title {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="centered-title">Event Attendees Report</h1>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered centered-table">
+            <thead>
                 <tr>
-                    <td>
-                        <img src="{{ asset('images/events/' . $event->image_url) }}" alt="user" class="rounded-circle"
-                            width="40">
-                    </td>
-                    <td>{{ $event->title }}</td>
-                    <td>{{ $event->organizer->name }}</td>
-                    <td>{{ $event->start_date }}</td>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Organizer</th>
+                    <th>Date</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @foreach($events as $event)
+                    <tr>
+                        <td>
+                            <img src="{{ asset('images/events/' . $event->image_url) }}" alt="user" class="rounded-circle" width="40">
+                        </td>
+                        <td>{{ $event->title }}</td>
+                        <td>{{ $event->organizer->name }}</td>
+                        <td>{{ $event->start_date }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
