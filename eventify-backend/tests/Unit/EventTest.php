45b23dc09f3567863bfe1806279cb8a0cb818452
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2025-01-21 19:42:23 +0100

    Create / Delete event unit tests and fixed event validation

--- eventify-backend/tests/Unit/EventTest.php
+++ eventify-backend/tests/Unit/EventTest.php
@@ -6,11 +6,14 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Event;
 use Illuminate\Support\Facades\Hash;
 
 class EventTest extends TestCase
 {
     use RefreshDatabase;
+
+    ###-------- EVENT CREATION TESTS --------###
 
     # Test to check if an organizer can create an event
     # Assertion 1: The application redirects the user to /events
@@ -18,7 +21,6 @@ class EventTest extends TestCase
     public function test_create_event()
     {
         $user = $this->createOrganizerUser();
-
         $category = $this->createCategory();
 
         $eventData = $this->getEventData();
@@ -31,9 +33,7 @@ class EventTest extends TestCase
         $this->assertDatabaseHas('events', $this->getEventData());
 
         # Clean up created models
-        $this->deleteEvent($this->getEventData());
-        $user->delete();
-        $category->delete();
+        $this->clearModels($user, $category, $eventData['title']);
     }
 
     # Test to check if an organizer can create an event without an image
@@ -42,7 +42,6 @@ class EventTest extends TestCase
     public function test_create_event_without_image()
     {
         $user = $this->createOrganizerUser();
-
         $category = $this->createCategory();
 
         $eventData = $this->getNoImageEventData();
@@ -55,19 +54,17 @@ class EventTest extends TestCase
         $this->assertDatabaseHas('events', $this->getEventData());
 
         # Clean up created models
-        $this->deleteEvent($this->getEventData());
-        $user->delete();
-        $category->delete();
+        $this->clearModels($user, $category, $eventData['title']);
     }
 
     # Test to check if a normal user cannot create an event
     # Assertion 1: The user is redirected to /home
-    # Assertion 2: The event is not created in the database
+    # Assertion 2: The petition receives a 302 status code (Redirection due to not having the correct role)
+    # Assertion 3: The event is not created in the database
     # 302 Code: This code means that the user is being redirected to /home due to not having the correct role
     public function test_normal_user_cannot_create_event()
     {
         $user = $this->createUser();
-
         $category = $this->createCategory();
 
         $eventData = $this->getEventData();
@@ -75,13 +72,160 @@ class EventTest extends TestCase
         $eventData['category_id'] = $category->id;
 
         $response = $this->actingAs($user)->post('/events', $eventData);
+        $response->assertRedirect('/home');
         $response->assertStatus(302);
         $this->assertDatabaseMissing('events', $eventData);
 
         # Clean up created models
-        $user->delete();
-        $category->delete();
+        $this->clearModels($user, $category, null);
     }
+
+    # Test to check if an event can be created with invalid data
+    # Assertion 1: Some of the fields are invalid
+    # Assertion 2: The event is not created in the database
+    public function test_create_event_with_invalid_data()
+    {
+        $user = $this->createOrganizerUser();
+        $category = $this->createCategory();
+
+        $eventData = $this->getEventData();
+        $eventData['organizer_id'] = $user->id;
+        $eventData['category_id'] = $category->id;
+        $eventData['start_date'] = 'invalid date';
+        $eventData['end_date'] = 'invalid date';
+        $eventData['location'] = 35;
+
+        $response = $this->actingAs($user)->post('/events', $eventData);
+
+        $response->assertSessionHasErrors(['location', 'start_date', 'end_date']);
+        $this->assertDatabaseMissing('events', $eventData);
+
+        # Clean up created models
+        $this->clearModels($user, $category, null);
+    }
+
+    # Test to check if a guest cannot create an event
+    # Assertion 1: The user is redirected to /login
+    # Assertion 2: The petition receives a 302 status code (Redirection due to not being logged in)
+    # Assertion 3: The event is not created in the database
+    public function test_guest_cannot_create_event()
+    {
+        $organizer = $this->createOrganizerUser();
+        $category = $this->createCategory();
+
+        $eventData = $this->getEventData();
+        $eventData['organizer_id'] = $organizer->id;
+        $eventData['category_id'] = $category->id;
+
+        $response = $this->post('/events', $eventData);
+
+        $response->assertRedirect('/login');
+        $response->assertStatus(302);
+        $this->assertDatabaseMissing('events', ['title' => $eventData['title']]);
+
+        # Clean up created models
+        $this->clearModels($organizer, $category, null);
+    }
+
+
+
+
+    ###-------- EVENT DELETION TESTS --------###
+
+    # Test to check if an organizer can delete an event
+    # Due to our application design, the event is not actually deleted from the database so we will check if the event is marked as deleted
+    # Assertion 1: The event is marked as deleted in the database
+    public function test_delete_event()
+    {
+        $user = $this->createOrganizerUser();
+        $category = $this->createCategory();
+
+        $eventData = $this->getEventData();
+        $eventData['organizer_id'] = $user->id;
+        $eventData['category_id'] = $category->id;
+
+        $event = Event::create($eventData);
+
+        $this->actingAs($user)->delete('/events/' . $event->id);
+
+        $eventData['deleted'] = 1;
+        $this->assertDatabaseHas('events', $eventData);
+
+        # Clean up created models
+        $this->clearModels($user, $category, $event->title);
+    }
+
+    # Test to check if a normal user cannot delete an event
+    # Assertion 1: The user is redirected to /home
+    # Assertion 2: The petition receives a 302 status code (Redirection due to not having the correct role)
+    # Assertion 3: The event is still not marked as deleted in the database
+    # 302 Code: This code means that the user is being redirected to /home due to not having the correct role
+    public function test_normal_user_cannot_delete_event()
+    {
+        $organizer = $this->createOrganizerUser();
+        $user = $this->createUser();
+        $category = $this->createCategory();
+
+        $eventData = $this->getEventData();
+        $eventData['organizer_id'] = $organizer->id;
+        $eventData['category_id'] = $category->id;
+
+        $event = Event::create($eventData);
+
+        $response = $this->actingAs($user)->delete('/events/' . $event->id);
+        $response->assertRedirect('/home');
+        $response->assertStatus(302);
+        $this->assertDatabaseHas('events', $eventData);
+
+        # Clean up created models
+        $user->delete();
+        $this->clearModels($organizer, $category, $event->title);
+    }
+
+    # Test to check if an organizer can delete an event that does not exist
+    # Due to our application redirecting the user to index if the event does not exist, we will check if the user is redirected to index
+    # Assertion 1: The user is redirected to index
+    # Assertion 2: The petition receives a 302 status code
+    public function test_organizer_cannot_delete_not_existing_event()
+    {
+        $user = $this->createOrganizerUser();
+
+        $response = $this->actingAs($user)->delete('/events/1');
+        $response->assertRedirect('/');
+        $response->assertStatus(302);
+
+        # Clean up created models
+        $user->delete();
+    }
+
+    # Test to check if a guest cannot delete an event
+    # Assertion 1: The user is redirected to /login
+    # Assertion 2: The petition receives a 302 status code (Redirection due to not being logged in)
+    # Assertion 3: The event is not marked as deleted in the database
+    public function test_guest_cannot_delete_event()
+    {
+        $organizer = $this->createOrganizerUser();
+        $category = $this->createCategory();
+
+        $eventData = $this->getEventData();
+        $eventData['organizer_id'] = $organizer->id;
+        $eventData['category_id'] = $category->id;
+
+        $event = Event::create($eventData);
+
+        $response = $this->delete('/events/' . $event->id);
+
+        $response->assertRedirect('/login');
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('events', $eventData);
+
+        # Clean up created models
+        $this->clearModels($organizer, $category, $event->title);
+    }
+
+
+    ###-------- AUXILIARY FUNCTIONS --------###
 
     private function createUser()
     {
@@ -113,6 +257,14 @@ class EventTest extends TestCase
         ]);
     }
 
+    private function clearModels($user, $category, $title)
+    {
+        $user->delete();
+        $category->delete();
+        if ($title != null)
+            Event::where('title', $title)->delete();
+    }
+
     private function getEventData()
     {
         return [
@@ -142,10 +294,5 @@ class EventTest extends TestCase
             'max_attendees' => 100,
             'price' => 7.89,
         ];
-    }
-
-    private function deleteEvent($eventData)
-    {
-        \App\Models\Event::where('title', $eventData['title'])->delete();
     }
 }
